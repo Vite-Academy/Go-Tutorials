22678a5d5f837f8e168af7dcd93b0d518ca10923
SHakhzodTojiyev <tojiyevshakhzod@gmail.com>
2024-01-26 23:49:03 +0500

    done

--- Examples/Values/app.go
+++ Examples/Values/app.go
@@ -6,8 +6,8 @@ func main() {
 
     fmt.Println("go" + "lang")
 
-    fmt.Println("1+1 =", 1+1)
-    fmt.Println("7.0/3.0 =", 7.0/3.0)
+    fmt.Println("1+1 = ", 2)
+    fmt.Println("7.0/3.0 = ", 7.0/3.0)
 
     fmt.Println(true && false)
     fmt.Println(true || false)
